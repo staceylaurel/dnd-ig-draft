77c78f4bc01f379ed8ff90fb2bd509bca5ff3329
staceylaurel <64053039+staceylaurel@users.noreply.github.com>
2020-11-04 14:06:59 -0600

    webpack updates

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,14 +1,12 @@
 import {Router} from 'express';
 import campaignsRouter from './campaigns';
 import disscussionsRouter from './discussions';
-// import playerRouter from './player';
-// import players_campaignsRouter from './players_campaigns';
+import players_campaignsRouter from './players_campaigns';
 
 const router = Router();
 
 router.use('/campaigns', campaignsRouter);
 router.use('/discussions', disscussionsRouter);
-// router.use('/player', playerRouter);
-// router.use('/players_campaigns', players_campaignsRouter);
+router.use('/players_campaigns', players_campaignsRouter);
 
 export default router;
