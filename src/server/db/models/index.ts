fb332874e8d801e500c9039cc377569c67ce9332
przekwas <luke@covalence.io>
2020-11-05 13:52:39 -0600

    fixing model interfaces

--- src/server/db/models/index.ts
+++ src/server/db/models/index.ts
@@ -1,57 +1,38 @@
-
 export interface Campaigns {
-    id?: string;
-    userid?: number;
-    name?: string;
-    created_at?: string
-    affectedRows: number,
-    insertId: 0,
-    serverStatus: 2,
-    message: '\'Records:14  Duplicated: 0  Warnings: 0',
-    changedRows: 0
+	id?: string;
+	userid?: number;
+	name?: string;
+	created_at?: string;
 }
 
 export interface Discussions {
-    id?: number;
-    userid?: number;
-    campaignid?: string;
-    image_url: string;
-    caption?: string;
-    created_at?: string
-    affectedRows: 14,
-    insertId: 0,
-    message: '\'Records:14  Duplicated: 0  Warnings: 0',
-    changedRows: 0
-  }
-  
+	id?: number;
+	userid?: number;
+	campaignid?: string;
+	image_url: string;
+	caption?: string;
+	created_at?: string;
+}
 
 export interface Player {
-    id?: number;
-    userid?: string;
-    email?: string;
-    password: string;
-    role: number;
-    created_at?: string
-    affectedRows: 14,
-    insertId: 0,
-    message: '\'Records:14  Duplicated: 0  Warnings: 0',
-    changedRows: 0
+	id?: number;
+	userid?: string;
+	email?: string;
+	password: string;
+	role: number;
+	created_at?: string;
 }
 
 export interface Players_campaigns {
-    id?: number;
-    campaignid?: string;
-    created_at?: string
-    affectedRows: 14,
-    insertId: 0,
-    message: '\'Records:14  Duplicated: 0  Warnings: 0',
-    changedRows: 0
+	id?: number;
+	campaignid?: string;
+	created_at?: string;
 }
 
 export interface cannedResponse {
-    affectedRows: number,
-    insertId: 0,
-    serverStatus: 2,
-    message: '\'Records:14  Duplicated: 0  Warnings: 0',
-    changedRows: 0
-  }
+	affectedRows: number;
+	insertId: number;
+	serverStatus: number;
+	message: string;
+	changedRows: number;
+}
