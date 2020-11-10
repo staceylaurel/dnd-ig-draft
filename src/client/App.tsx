ec3986671d27b71005e646fb263709ea542c60a1
przekwas <luke@covalence.io>
2020-11-10 15:06:50 -0600

    fixes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,63 +1,62 @@
-//@ts-nocheck
 //library imports
-import * as React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import * as React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 //component imports
-import Home from "././views/Home";
-import DM from "././views/DM";
-import Player from "/./views/Player";
-import Login from "././views/Login";
-import Role from "././views/Role";
-import RegisterDM from "././views/RegisterDM";
-import RegisterPlayer from "././views/RegisterPlayer";
-import PrivateRoute from "./components/PrivateRoute";
-import Discussion from "./views/Discussion";
-import ListPlayers from "./views/ListPlayers";
-import Profile from "./views/Profile";
-import UploadDiscussion from "./views/UploadDiscussion";
+import Home from './views/entry/Home';
+import DM from './views/entry/DM';
+import Player from './views/entry/Player';
+import Login from './views/entry/Login';
+import Role from './views/entry/Role';
+import RegisterDM from './views/entry/RegisterDM';
+import RegisterPlayer from './views/entry/RegisterPlayer';
+import PrivateRoute from './utils/PrivateRoute';
+import Discussion from './views/Discussion';
+import ListPlayers from './views/ListPlayers';
+import Profile from './views/Profile';
+import UploadDiscussion from './views/UploadDiscussion';
 
-
-const App: React.FC<AppProps> = (props) => {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-		  </Route>
-		  <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/DM">
-          <DM />
-        </Route>
-        <Route exact path="/Player">
-          <Player />
-        </Route>
-        <Route exact path="/Role">
-          <Role />
-        </Route>
-        <Route exact path="/registerDM">
-          <RegisterDM />
-        </Route>
-		<Route exact path="/registerPlayer">
-          <RegisterPlayer />
-		  </Route>
-        <PrivateRoute exact path="/Discussion">
-          <Discussion />
-        </PrivateRoute>
-        <PrivateRoute exact path="/ListPlayers">
-          <ListPlayers />
-        </PrivateRoute>
-		<PrivateRoute exact path="/Profile">
-          <Profile />
-        </PrivateRoute>
-		<PrivateRoute exact path="/UploadDiscussion">
-          <UploadDiscussion />
-        </PrivateRoute>
-      </Switch>
-    </BrowserRouter>
-  );
+const App: React.FC<AppProps> = props => {
+	return (
+		<BrowserRouter>
+			{/* <NavBar /> */}
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/login">
+					<Login />
+				</Route>
+				<Route exact path="/DM">
+					<DM />
+				</Route>
+				<Route exact path="/Player">
+					<Player />
+				</Route>
+				<Route exact path="/Role">
+					<Role />
+				</Route>
+				<Route exact path="/registerDM">
+					<RegisterDM />
+				</Route>
+				<Route exact path="/registerPlayer">
+					<RegisterPlayer />
+				</Route>
+				<PrivateRoute exact path="/Discussion">
+					<Discussion />
+				</PrivateRoute>
+				<PrivateRoute exact path="/ListPlayers">
+					<ListPlayers />
+				</PrivateRoute>
+				<PrivateRoute exact path="/Profile">
+					<Profile />
+				</PrivateRoute>
+				<PrivateRoute exact path="/UploadDiscussion">
+					<UploadDiscussion />
+				</PrivateRoute>
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 interface AppProps {}
--- src/client/views/UploadDiscussion.tsx
+++ src/client/views/UploadDiscussion.tsx
@@ -1,16 +1,15 @@
+import * as React from 'react';
 
+const Template: React.FC<TemplateProps> = props => {
+	return (
+		<div>
+			<h1> Template</h1>
+		</div>
+	);
+};
 
+interface TemplateProps {}
 
-
-
-
-
-
-
-
-
-
-
-
+export default Template;
 
 //Add image and caption
